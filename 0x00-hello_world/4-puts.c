fe1814035b14180ad06b242c4cf21452d625e27e
Abdulrahman Nasser Siddiq <abdulrahman.nasser@gmail.com>
2023-02-17 07:51:28 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ *main -  this is main function
+ *Return - end with zero
+ */
 int main(void)
 {
+	/*this print the line Programing...*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		return (0);
 }
