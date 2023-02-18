ee1a2825ad2711658ab5ecc2412f8fe16b7339f5
Abdulrahman Nasser Siddiq <abdulrahman.nasser@gmail.com>
2023-02-18 02:06:10 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -print if func
+ * return zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/*return zero*/
+	return (0);
+	/*retun func*/
+}
